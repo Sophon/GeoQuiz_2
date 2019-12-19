ab71f5c5e7471d21edc600e3f0d8522ac0975ee9
Le Duy Anh <lenduya@gmail.com>
2019-12-19 01:15:21 +0100

    Create QuizViewModel

--- app/src/main/java/com/bignerdranch/android/geoquiz_2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz_2/MainActivity.kt
@@ -2,8 +2,19 @@ package com.bignerdranch.android.geoquiz_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var falseButton: Button
+    private lateinit var trueButton: Button
+    private lateinit var previousButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/bignerdranch/android/geoquiz_2/QuizViewModel.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.geoquiz_2
+
+import androidx.lifecycle.ViewModel
+
+class QuizViewModel: ViewModel() {
+
+}
