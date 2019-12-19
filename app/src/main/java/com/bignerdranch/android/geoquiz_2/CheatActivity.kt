c5fa71918873c7dcc957800f009e04caea728fbc
Le Duy Anh <lenduya@gmail.com>
2019-12-19 23:20:02 +0100

    Create CheatViewModel

--- app/src/main/java/com/bignerdranch/android/geoquiz_2/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz_2/CheatActivity.kt
@@ -4,10 +4,17 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProviders
 
 private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var showAnswerButton: Button
+
+    private val cheatViewModel: CheatViewModel by lazy {
+        ViewModelProviders.of(this).get(CheatViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
