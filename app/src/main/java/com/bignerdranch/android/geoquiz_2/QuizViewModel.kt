5f5088eaaf0d41c8af35bc48cfc144481593102f
Le Duy Anh <lenduya@gmail.com>
2019-12-19 01:27:28 +0100

    Implement QuizViewModel

--- app/src/main/java/com/bignerdranch/android/geoquiz_2/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz_2/QuizViewModel.kt
@@ -3,5 +3,31 @@ package com.bignerdranch.android.geoquiz_2
 import androidx.lifecycle.ViewModel
 
 class QuizViewModel: ViewModel() {
+    val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
 
+    var currentIndex = 0
+    var isCheater = false
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToPreviousQuestion() {
+        if(currentIndex != 0) {
+            currentIndex -= 1
+        }
+    }
+
+    fun moveToNextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
 }
